5d550c797560a24affd4ce86e249f428cb3f25db
Giacomo Voß <giacomo.voss@campuscore.eu>
2021-02-10 07:35:44 +0100

    0.4.0

--- src/modules/core/view/abstract-view-controller.ts
+++ src/modules/core/view/abstract-view-controller.ts
@@ -2,10 +2,12 @@ import {AbstractController} from "../router";
 import {DataObject} from "../model";
 import {Enemene, ViewInitializerService, ViewService} from "../../..";
 import {ViewDefinition} from "./class/view-definition.class";
+import {ViewHelperService} from "./service/view-helper.service";
 
 export class AbstractViewController extends AbstractController {
 
     protected viewService: ViewService = Enemene.app.inject(ViewService);
+    protected viewHelperService: ViewHelperService = Enemene.app.inject(ViewHelperService);
 
     protected getViewDefinition<ENTITY extends DataObject<ENTITY>>(viewName: string): ViewDefinition<ENTITY> {
         return ViewInitializerService.getViewDefinition(viewName);
